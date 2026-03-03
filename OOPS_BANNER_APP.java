07fe9e02a63a10844f7e4381aac2c1a8d632c65f
samaisouparna81-cmd <samaisouparna81@gmail.com>
2026-03-03 17:58:19 +0530

    Create OOPS_BANNER_APP.java

---
+++ OOPS_BANNER_APP.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC2: Display "OOPS" in large ASCII banner format
+ * using '*' and spaces with manual print statements.
+ * 
+ * @author Souparna
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println(" *****   *****   *      *****  ");
+    }
+}
